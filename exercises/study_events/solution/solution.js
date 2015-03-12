e321eb334c8293c6577b388b7be4940c03ae0fe4
Mike Grabowski <grabbou@gmail.com>
2015-03-12 09:09:19 +0100

    Working solution for exercise 1

--- exercises/study_events/solution/solution.js
+++ exercises/study_events/solution/solution.js
@@ -1,1 +1,10 @@
-// solution stuff here
+var EventEmitter = require('events').EventEmitter;
+var em = new EventEmitter();
+var events = process.argv.slice(2);
+
+events.forEach(function setupListeners(event) {
+  em.on(event, function() {
+    console.log(event, 'has been received');
+  });
+  em.emit(event);
+});
