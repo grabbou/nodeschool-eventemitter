d0aba0b4615699ff33cd53d2d9e1ba703370ce82
Mike Grabowski <grabbou@gmail.com>
2015-03-12 13:07:18 +0100

    here comes solution

--- exercises/study_events_3/solution/solution.js
+++ exercises/study_events_3/solution/solution.js
@@ -1,1 +1,13 @@
-// solution stuff here
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = function(maximumNumberOfListeners) {
+  var lecture = new EventEmitter();
+
+  lecture.setMaxListeners(maximumNumberOfListeners);
+
+  lecture.once('newListener', function() {
+    this.emit('start', 'How to be a successful manager?');
+  });
+
+  return lecture;
+};
